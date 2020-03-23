84be143f15eb1d59cd2caf30bf08483417788c1d
Daniel Mox <mox@seas.upenn.edu>
2020-03-23 17:24:59 -0400

    move connectivity maximization implementation to ConnectivityOpt class

--- connectivity_maximization.py
+++ connectivity_maximization.py
@@ -18,15 +18,84 @@ def circle_points(rad, num_points):
     return pts
 
 
-class GraphConnectivity:
-    def __init__(self, print_values=True, n0=-70.0, n=2.52, l0=-53.0, a=0.2, b=6.0):
+class ConnectivityOpt:
+    def __init__(self, print_values=True, n0=-70.0, n=2.52, l0=-53.0, a=0.2, b=6.0,
+                 x_task=None, x_comm=None):
         self.cm = ChannelModel(print_values=print_values, n0=n0, n=n, l0=l0, a=a, b=b)
+        self.x_task = x_task
+        self.x_comm = x_comm
+        if x_task is not None and x_comm is not None:
+            self.config = np.vstack((self.x_task, self.x_comm))
+            self.agent_count = self.config.shape[0]
+            self.comm_count = self.x_comm.shape[0]
+            self.comm_idcs = range(self.x_task.shape[0], self.agent_count)
 
-    def compute_connectivity(self, x):
+    def connectivity(self, x):
         rate, _ = self.cm.predict(x)
         lap = np.diag(np.sum(rate, axis=1)) - rate
         v, _ = np.linalg.eigh(lap)
         return v[1]
+
+    # TODO use cp.parameter?
+    def update_network_config(self, step_size):
+
+        gamma = cp.Variable((1))
+        x = cp.Variable((self.comm_count, 2))
+        Aij = cp.Variable((self.agent_count, self.agent_count))
+        Lij = cp.Variable((self.agent_count, self.agent_count))
+
+        # relative closeness
+
+        constraints = [cp.norm(x - self.config[self.comm_idcs], 1) <= step_size * self.comm_count]
+
+        # linearized rate model
+
+        for i in range(self.agent_count):
+            for j in range(self.agent_count):
+                if i == j:
+                    constraints += [Aij[i,j] == 0.0]
+                    continue
+                xi = self.config[i,:]
+                xj = self.config[j,:]
+                Rxixj, _ = self.cm.predict_link(xi, xj)
+                dRdxi = self.cm.derivative(xi, xj)
+                dRdxj = self.cm.derivative(xj, xi)
+                if i in self.comm_idcs and j in self.comm_idcs:
+                    xi_var = x[self.comm_idcs.index(i),:]
+                    xj_var = x[self.comm_idcs.index(j),:]
+                    constraints += [Aij[i,j] == Rxixj + dRdxi.T * (xi_var - xi).T \
+                                                      + dRdxj.T * (xj_var - xj).T]
+                elif i in self.comm_idcs:
+                    xi_var = x[self.comm_idcs.index(i),:]
+                    constraints += [Aij[i,j] == Rxixj + dRdxi.T * (xi_var - xi).T]
+                elif j in self.comm_idcs:
+                    xj_var = x[self.comm_idcs.index(j),:]
+                    constraints += [Aij[i,j] == Rxixj + dRdxj.T * (xj_var - xj).T]
+                else:
+                    constraints += [Aij[i,j] == Rxixj]
+
+        # graph laplacian
+
+        constraints += [Lij == cp.diag(cp.sum(Aij, axis=1)) - Aij]
+
+        # 2nd smallest eigen value
+
+        P = null_space(np.ones((1, self.agent_count)))
+        constraints += [P.T * Lij * P >> gamma*np.eye(self.agent_count-1)]
+
+        #
+        # solve problem
+        #
+
+        prob = cp.Problem(cp.Maximize(gamma), constraints)
+        prob.solve()
+
+        if prob.status is not 'optimal':
+            print(prob.status)
+
+        self.x_comm = x.value
+        self.config[self.comm_idcs] = self.x_comm
+        return self.connectivity(self.config)
 
 
 def channel_derivative_quiver():
@@ -55,12 +124,12 @@ def connectivity_distance_test():
 
     x_task = circle_points(task_rad, 3)
 
-    gc = GraphConnectivity(print_values=False)
+    co = ConnectivityOpt(print_values=False)
     rad = np.linspace(0.1, 20.0, num=lambda2_points, endpoint=False)
     lambda2 = np.zeros_like(rad)
     for i in range(lambda2_points):
         x_comm = circle_points(rad[i], comm_agent_count)
-        lambda2[i] = gc.compute_connectivity(np.vstack((x_task, x_comm)))
+        lambda2[i] = co.connectivity(np.vstack((x_task, x_comm)))
 
     fig, ax = plt.subplots()
     ax.plot(rad, lambda2)
@@ -98,100 +167,33 @@ def derivative_test():
     plt.show()
 
 
-# TODO use cp.parameter
-def local_controller(gc, x0, comm_agent_idcs, step_size):
-
-    agent_count = x0.shape[0]
-    comm_agent_count = len(comm_agent_idcs)
-    task_agent_count = agent_count - comm_agent_count
-
-    gamma = cp.Variable((1))
-    x = cp.Variable((comm_agent_count,2))
-    Aij = cp.Variable((agent_count, agent_count))
-    Lij = cp.Variable((agent_count, agent_count))
-
-    # relative closeness
-
-    constraints = [cp.norm(x - x0[comm_agent_idcs], 1) <= step_size * comm_agent_count]
-
-    # linearized rate model
-
-    for i in range(agent_count):
-        for j in range(agent_count):
-            if i == j:
-                constraints += [Aij[i,j] == 0.0]
-                continue
-            xi = x0[i,:]
-            xj = x0[j,:]
-            Rxixj, _ = gc.cm.predict_link(xi, xj)
-            dRdxi = gc.cm.derivative(xi, xj)
-            dRdxj = gc.cm.derivative(xj, xi)
-            if i in comm_agent_idcs and j in comm_agent_idcs:
-                xi_var = x[comm_agent_idcs.index(i),:]
-                xj_var = x[comm_agent_idcs.index(j),:]
-                constraints += [Aij[i,j] == Rxixj + dRdxi.T * (xi_var - xi).T \
-                                                  + dRdxj.T * (xj_var - xj).T]
-            elif i in comm_agent_idcs:
-                xi_var = x[comm_agent_idcs.index(i),:]
-                constraints += [Aij[i,j] == Rxixj + dRdxi.T * (xi_var - xi).T]
-            elif j in comm_agent_idcs:
-                xj_var = x[comm_agent_idcs.index(j),:]
-                constraints += [Aij[i,j] == Rxixj + dRdxj.T * (xj_var - xj).T]
-            else:
-                constraints += [Aij[i,j] == Rxixj]
-
-    # graph laplacian
-
-    constraints += [Lij == cp.diag(cp.sum(Aij, axis=1)) - Aij]
-
-    # 2nd smallest eigen value
-
-    P = null_space(np.ones((1, agent_count)))
-    constraints += [P.T * Lij * P >> gamma*np.eye(agent_count-1)]
-
-    #
-    # solve problem
-    #
-
-    prob = cp.Problem(cp.Maximize(gamma), constraints)
-    prob.solve()
-
-    if prob.status is not 'optimal':
-        print(prob.status)
-
-    x0[comm_agent_idcs] = x.value
-
-    return gc.compute_connectivity(x0)
-
-
 def algebraic_connectivity_sdp():
 
-    gc = GraphConnectivity(print_values=False)
-
-    x0 = np.asarray([[0.0, 0.0], [10.0, 20.0], [20.0, 0.0], [-2.0, 5.0], [25.0, 10.0]])
-    # x0 = np.asarray([[0.0, 0.0], [10.0, 20.0], [20.0, 0.0], [2.0, 0.0], [4.0, 0.0]])
-    comm_agent_idcs = [3, 4]
-    # x0 = np.asarray([[0.0, 0.0], [10.0, 20.0], [20.0, 0.0], [2.0, 0.0], [4.0, 0.0], [20.0, 10.0]])
-    # comm_agent_idcs = [3, 4, 5]
+    x_task = np.asarray([[0.0, 0.0], [10.0, 20.0], [20.0, 0.0]])
+    x_comm = np.asarray([[-2.0, 5.0], [25.0, 10.0]])
+    # x_comm = np.asarray([[2.0, 0.0], [4.0, 0.0]])
+    # x_comm = np.asarray([[2.0, 0.0], [4.0, 0.0], [20.0, 10.0]])
     step_size = 0.2
 
+    co = ConnectivityOpt(print_values=False, x_task=x_task, x_comm=x_comm)
+
     fig, ax = plt.subplots()
-    plot_config(numpy_to_ros(x0), ax=ax, show=True, pause=1.0,
-                title="lambda 2 = {:.3f}".format(gc.compute_connectivity(x0)))
+    plot_config(numpy_to_ros(np.vstack((x_task, x_comm))), ax=ax, show=True, pause=1.0,
+                title="lambda 2 = {:.3f}".format(co.connectivity(np.vstack((x_task, x_comm)))))
 
     update = 1.0
     lambda20 = 0.0
     it = 1
     while (update > 1e-10):
-        lambda2 = local_controller(gc, x0, comm_agent_idcs, step_size)
-        plot_config(numpy_to_ros(x0), ax=ax, clear_axes=True, show=True, pause=0.01,
+        lambda2 = co.update_network_config(step_size)
+        plot_config(numpy_to_ros(co.config), ax=ax, clear_axes=True, show=True, pause=0.01,
                     title="it: {:3d}, lambda 2 = {:.3f}".format(it, lambda2))
         update = lambda2 - lambda20
         lambda20 = lambda2
         it += 1
 
     print("best lambda2 = {:.6f}".format(lambda2))
-    plot_config(numpy_to_ros(x0), ax=ax, clear_axes=True, show=True,
+    plot_config(numpy_to_ros(co.config), ax=ax, clear_axes=True, show=True,
                 title="total its: {:3d}, lambda 2 = {:.3f}".format(it-1, lambda2))
 
 if __name__ == '__main__':
